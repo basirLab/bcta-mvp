ead192ef49d2ff1bb9d5f4d2fcb8af49d19b735a
BasirLAB <founder.basirlab@gmail.com>
2025-06-21 03:50:50 +0900

    Update evaluate.js

--- evaluate.js
+++ evaluate.js
@@ -39,7 +39,7 @@ export default async function handler(req, res) {
             'Content-Type': 'application/json'
           },
           body: JSON.stringify({
-            model: 'gpt-4',
+            model: 'gpt-4-turbo',
             messages: [{ role: 'user', content: prompt }]
           })
         });
